b5cf03316433e857cc290eaeac952d1944a1d5cb
huaxin <57068786+huaxinCLUB@users.noreply.github.com>
2022-03-16 19:33:38 +0800

    without Wi-Fi patch

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git opicyberwrt https://github.com/melsem/opi-zero-cyberwrt.git' >>feeds.conf.default
+#echo 'src-git opicyberwrt https://github.com/melsem/opi-zero-cyberwrt.git' >>feeds.conf.default
